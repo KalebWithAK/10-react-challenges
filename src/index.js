29ad62abefb0386844a552813ed81382ea3ef679
KalebWithAK <kad9603@gmail.com>
2022-10-12 02:28:45 -0400

    added hash router to fix routing error

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 
 import Home from './pages/home'
 import UsersArray from './pages/usersArray'
 import HideShow from './pages/hideShow'
 import DataBinding from './pages/dataBinding'
-import ErrorPage from './pages/errorPage'
 import DisableButton from './pages/disableButton'
 import UpdateParent from './pages/updateParent'
 import AddChild from './pages/addChild'
@@ -15,8 +14,7 @@ import Sum from './pages/sum'
 import Counter from './pages/counter'
 import Fetch from './pages/fetch'
 
-const router = createBrowserRouter([
-  { path: '/', element: <Home />, errorElement: <ErrorPage /> },
+const paths = [
   { path: '/array', element: <UsersArray /> },
   { path: '/show-hide', element: <HideShow /> },
   { path: '/data-bind', element: <DataBinding /> },
@@ -26,11 +24,16 @@ const router = createBrowserRouter([
   { path: '/sum', element: <Sum /> },
   { path: '/counter', element: <Counter /> },
   { path: '/fetch', element: <Fetch /> }
-])
+]
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Router>
+      <Switch>
+        <Route exact path='/'><Home /></Route>
+        {paths.map(i => <Route path={i.path}>{i.element}</Route>)}
+      </Switch>
+    </Router>
   </React.StrictMode>
 )
